0687cb4ede7d4eaa5cb346f73f6158c1ba888ea4
r.malinovskiy <r.malinovskiy@t-point.dev>
2022-06-29 09:53:20 +0300

    Заготовка для клиента

---
+++ AreaLibraryClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AreaLibrary;
+
+namespace AreaLibraryClient
+{
+	public static class Program
+	{
+		private static void Main(string[] args)
+		{
+			var class1 = new Class1();
+			class1.SayHello();
+		}
+	}
+}
